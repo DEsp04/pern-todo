4c0466404c6df38263af0b253e2506a236d6fa51
David Espinal <despinal0425@gmail.com>
2021-09-11 15:10:12 -0400

    data can be added to the database from client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import InputTodo from './components/InputTodo';
 
 function App() {
   return (
     <div className="App">
-      
+      <InputTodo />
     </div>
   );
 }
--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function InputTodo() {
+
+  const [description, setDescription] = useState("")
+
+  //submit form to send data out
+  const onSubmitForm = async (e) => {
+    //preven form from refreshing
+    e.preventDefault()
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5000/api/todos", {
+        method: "POST",
+        //sending json data to the server
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+        
+      })
+
+      console.log(response)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+  
+
   return (
     <div>
-      
+      <h1 className="text-center mt-5">Pern Todo List</h1>
+
+      <form className="d-flex mt-5 mx-4" onSubmit={onSubmitForm}>
+        <input type="text" className="form-control" value={description} onChange={ e => setDescription(e.target.value)} />
+        <button className="btn btn-success">Add</button>
+      </form>
     </div>
   )
 }
