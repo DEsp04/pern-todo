a075f872dd88f5098e238d5736a29579dfe8affa
David Espinal <despinal0425@gmail.com>
2021-09-12 19:16:47 -0400

    list can now be render on the client side

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -7,8 +7,29 @@ import React, {useEffect, useState} from 'react'
 
 function ListTodos() {
 
-  
+  const [todos, setTodos] = useState([])
 
+  const getTodos = async () => {
+    try {
+      
+      const response = await fetch("http://localhost:5000/api/todos")
+
+      //parse the data
+      const jsonData = await response.json()
+
+      setTodos(jsonData);
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+
+  useEffect(() => {
+    getTodos();
+  }, [])  
+
+  // console.log(todos)
 
   return (
     <div>
@@ -26,7 +47,13 @@ function ListTodos() {
             <td>Doe</td>
             <td>john@example.com</td>
           </tr> */}
-          
+          {todos.map(todo => (
+            <tr>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
